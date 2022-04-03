43690702a87bc12bb9496b6e1cc57d567f5fef37
Roy Kim <royjayshkim2@gmail.com>
2022-04-03 00:19:24 -0500

    modularize the code

--- src/components/Banner.js
+++
@@ -1,5 +0,0 @@
-const Banner = ({ title }) => (
-    <h1>{title}</h1>
-);
-
-export default Banner
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,23 +1,47 @@
+import React, { useState } from 'react';
+import { terms, getCourseTerm } from '../utilities/times.js';
 import Course from './Course.js'
-import TermSelector from './TermSelector.js'
 
-const TermSelector = () => (
-  <div className="btn-group">
-  { 
-    Object.values(terms)
-      .map(value => <TermButton key={value} term={value} />)
-  }
-  </div>
-);
 
-const CourseList = ({ courses }) => (
-  <>
-    <TermSelector />
-    <div className="course-list">
-      {Object.values(courses).map(course => <Course key={course.id} course={course} />)}
+const TermSelector = ({ term, setTerm }) => (
+    <div className="btn-group">
+        {
+            Object.values(terms).map(value => (
+                <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+            ))
+        }
     </div>
-  </>
-  // JSX syntax only allows one component to be returned.
-  // The empty element syntax <> is a way to return several components as one, without creating an unnecessary HTML element, such as a div.
 );
-export default CourseList
+
+const TermButton = ({ term, setTerm, checked }) => (
+    <>
+        <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+            onChange={() => setTerm(term)} />
+        <label className="btn btn-success m-1 p-2" htmlFor={term}>
+            {term}
+        </label>
+    </>
+);
+
+
+const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+    return (
+        <>
+            <TermSelector term={term} setTerm={setTerm} />
+            <div className="course-list">
+                {
+                    termCourses.map(course =>
+                        <Course key={course.id} course={course}
+                            selected={selected} setSelected={setSelected}
+                        />)
+                }
+            </div>
+        </>
+    );
+};
+
+export default CourseList;
