4d82d226dc22db909bfcba4845d7a999b7cf01d1
Roy Kim <royjayshkim2@gmail.com>
2022-04-03 22:22:05 -0500

    read from firebase database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAk1xbo_K7kUksQscTD5vrXDHeDnAobpi8",
+    authDomain: "react-tutorial-c13ad.firebaseapp.com",
+    databaseURL: "https://react-tutorial-c13ad-default-rtdb.firebaseio.com",
+    projectId: "react-tutorial-c13ad",
+    storageBucket: "react-tutorial-c13ad.appspot.com",
+    messagingSenderId: "323594398892",
+    appId: "1:323594398892:web:4899ccd291f477f547c309",
+    measurementId: "G-L4PKCC52JX"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+        if (devMode) { console.log(`loading ${path}`); }
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            if (devMode) { console.log(val); }
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+
+    return [data, loading, error];
+};
